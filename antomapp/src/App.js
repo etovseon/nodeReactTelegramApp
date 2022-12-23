1aeeb58dd4efac75365d0f442b42f5f6216fbdba
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-23 11:14:47 +0200

    fixOnClose

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -5,11 +5,12 @@ const tg = window.Telegram.WebApp;
 
 function App() {
 
-//    useEffect(() => {
-//      tg.ready();
-//    },[])
-      const onClose = () => {
-      tg.close();
+
+  //useEffect(() => {
+  //tg.ready();
+  //},[])
+  const onClose = () => {
+  tg.close();
 }
   return (
     <div className="App">
