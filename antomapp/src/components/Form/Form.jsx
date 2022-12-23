e842223a053afcf1e9e07199e072b6d469517d70
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-23 14:57:42 +0200

    add form row

--- antomapp/src/components/Form/Form.jsx
+++ antomapp/src/components/Form/Form.jsx
@@ -4,7 +4,16 @@ import './Form.css'
 const Form = () => {
     return (
         <div>
-            Form!
+            <div className={"form"}>
+                <h3>Введите ваши данные</h3>
+                <input className={'input'} type="text" placeholder={'Страна'}></input>
+                <input className={'input'} type="text" placeholder={'Улица'}></input>
+                <br />
+            <select>
+                <option value={'legal'}>Юр.лицо</option>
+                <option value={'physical'}>Физ.лицо</option>
+            </select>
+            </div>
         </div>
     )
 }
