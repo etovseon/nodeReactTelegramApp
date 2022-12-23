1f70efa4d780f8f7428ddb59f06990d0f4f1e5cd
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-23 13:09:08 +0200

    add toogle

---
+++ antomapp/src/components/hooks/useTelegram.js
@@ -0,0 +1,23 @@
+//const tg = window.Telegram.WebApp;
+
+export function useTelegram() {
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close()
+    }
+
+    const onTogleButton = () => {
+        if (tg.MainButton.isVisible)
+        {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
+    return {
+        onClose,
+        onTogleButton,
+        tg,
+        user: tg.initDateUnsafe?.user,
+    }
+}
