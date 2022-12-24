b00c909f32c8652c1085769b09a7c93b532631d5
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-24 17:07:37 +0200

    e

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -22,6 +22,10 @@ function App() {
       <Routes>
         <Route index element={<ProductList/>}/>
         <Route path={'form'} element={<Form/>}/>
+        <Route path="form" element={<Form/>}/>
+        <Route path={'/form'} element={<Form/>}/>
+        <Route path='/form' element={<Form/>}/>
+        
       </Routes>
     </div>
   );
