c2a3c27e8682fc76c498d0305835874f024f6de0
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-24 17:47:11 +0200

    a

---
+++ antomapp/src/ProductList2/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './ProductList.css'
+
+const ProductList = () => {
+    return (
+        <div>
+            ProductList Menu
+        </div>
+    )
+}
+
+export default ProductList;
