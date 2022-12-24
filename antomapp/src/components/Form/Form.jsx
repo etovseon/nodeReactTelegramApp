2b89f4fba9a0d3fd675c5e825fa67d9e518880b5
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-24 13:45:04 +0200

    rm br

--- antomapp/src/components/Form/Form.jsx
+++ antomapp/src/components/Form/Form.jsx
@@ -39,7 +39,6 @@ const Form = () => {
                 <h3>Введите ваши данные</h3>
                 <input className={'input'} type="text" placeholder={'Страна'} value={country} onChange={onChangeCountry}></input>
                 <input className={'input'} type="text" placeholder={'Улица'} value={street} onChange={onChangeStreet}></input>
-                <br />
                 <select value={subject} onChange={onChangeSubject} className={'select'} >
                 <option value={'legal'} >Юр.лицо</option>
                 <option value={'physical'}>Физ.лицо</option>
