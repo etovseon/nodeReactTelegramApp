1ee3e578965decead9be08f2c9e269558d3e23f4
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-24 17:15:18 +0200

    change route fix

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       <Header/>
         <Routes>
-
+          <Route index element={<ProductList/>}/>
           <Route index element={<Form/>}/>
 
         
