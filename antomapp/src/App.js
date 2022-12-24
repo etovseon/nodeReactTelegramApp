60e443a8d3de609e7e54f7b200da2e053f182467
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-24 17:18:07 +0200

    fix route add import path

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import {Route, Routes} from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
+import { Path } from 'react-router-dom';
 
 
 
@@ -21,7 +22,7 @@ function App() {
       <Header/>
         <Routes>
           <Route index element={<ProductList/>}/>
-          <Route index element={<Form/>}/>
+          <Route path='form' element={<Form/>}/>
 
         
         </Routes>
