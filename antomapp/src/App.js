042bdb4e100a40c143603c221bb5432ad58c24b0
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-24 12:41:09 +0200

    fix notlefy toml file route

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -14,14 +14,14 @@ function App() {
   useEffect(() => {
     tg.ready();
   },[])
-
+  
   
   return (
     <div className="App">
       <Header/>
       <Routes>
         <Route index element={<ProductList/>}/>
-        <Route path={'/form'} element={<Form/>}/>
+        <Route path={'form'} element={<Form/>}/>
       </Routes>
     </div>
   );
