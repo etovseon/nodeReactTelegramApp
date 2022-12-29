a59b5780f0f1c3130c193745787783c0780b5157
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-29 10:05:15 +0200

    dele pack

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 import {Route, Routes} from 'react-router-dom';
 import ProductList from './ProductList/ProductList';
 import Form from './components/Form/Form';
-import {BrowserRouter as Router} from 'react-router-dom';
+
 function App() {
 
   const {onTogleButton,tg} = useTelegram();
