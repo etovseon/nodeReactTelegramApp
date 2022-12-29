b8bc4c95c8ac1ac5487b33ec1ecdbeebc2dbc6d2
nodeReactTelegramApp <shasmoke2@gmail.com>
2022-12-29 02:21:42 +0200

    3

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 import {Route, Routes} from 'react-router-dom';
 import ProductList from './ProductList/ProductList';
 import Form from './components/Form/Form';
-import {BrowserRouter as Router} from 'react-router-dom';
+
 function App() {
 
   const {onTogleButton,tg} = useTelegram();
@@ -19,8 +19,9 @@ function App() {
     <div className="App">
       <Header/>
       <Routes>
+      <Route path='form' element={<ProductList/>}/>
         <Route index element={<Form/>}/>
-        <Route path='form' element={<ProductList/>}/>
+        
       </Routes>
     </div>
   );
