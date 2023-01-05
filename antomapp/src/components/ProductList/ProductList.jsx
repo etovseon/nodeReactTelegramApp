51a7f409a7a03768a3a440c96fb9c17285300696
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-06 00:04:56 +0200

    5s

--- antomapp/src/components/ProductList/ProductList.jsx
+++ antomapp/src/components/ProductList/ProductList.jsx
@@ -11,7 +11,7 @@ fetch(sendTel2+'started')
 
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '1', title: 'Джинсы6', price: 5000, description: 'Синего цвета, прямые'},
     {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
     {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
     {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
