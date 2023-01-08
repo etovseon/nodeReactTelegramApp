80f523a535dfb81d719a92f9ed343dc77fb0d5ae
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-08 20:28:59 +0200

    add menu list

--- antomapp/src/components/first_frame/first_frame.js
+++ antomapp/src/components/first_frame/first_frame.js
@@ -3,7 +3,9 @@ const First_frame  = () => {
     <div>
         <ul>
             <li><a href='/order'order></a></li>
-            <li><a href='/last_orer'>last order</a></li>    
+            <li><a href='/last_orer'>last order</a></li>
+            <li><a href='/'>Product List</a></li>
+            <li><a href='/form'>Анкета</a></li>
         </ul>
     </div>
     )
