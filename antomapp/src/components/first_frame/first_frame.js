12037819c13ea2c61561aea4ed309c3fbd0f7550
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-08 20:35:02 +0200

    fix

--- antomapp/src/components/first_frame/first_frame.js
+++ antomapp/src/components/first_frame/first_frame.js
@@ -2,12 +2,12 @@ const First_frame  = () => {
     return (
     <div>
         <ul>
-            <li><a href='/order'order></a></li>
+            <li><a href='/order'>order</a></li>
             <li><a href='/last_orer'>last order</a></li>
             <li><a href='/'>Product List</a></li>
             <li><a href='/form'>Анкета</a></li>
         </ul>
     </div>
-    )
+    );
 }
 export default First_frame;
