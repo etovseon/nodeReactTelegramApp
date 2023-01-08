7b6fe38dcd9855c10955346aa9283c4d33357864
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-08 20:20:28 +0200

    test menu route

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
 import { useTelegram } from './components/hooks/useTelegram';
+import {first_frame} from './components/first_frame/first_frame'
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
       <Header/>
       <Routes>
         <Route index element={<ProductList/>}/>
+        <Route path='menu' element={<first_frame/>}/>
         <Route path='form' element={<Form/>}/>
       </Routes>
     </div>
---
+++ antomapp/src/components/first_frame/first_frame.js
@@ -0,0 +1,13 @@
+const first_frame  = () => {
+    return (
+        <ul>
+            
+                
+                    <li><a href='/order'order></a></li>
+                    <li><a href='/last_orer'>last order</a></li>
+                
+            
+        </ul>
+    )
+}
+export default first_frame;
