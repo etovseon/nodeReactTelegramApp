b1713b05bc05716669c2354468898d2fb7553edf
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-08 20:40:59 +0200

    fix

--- antomapp/src/App.js
+++ antomapp/src/App.js
@@ -5,7 +5,7 @@ import {Route, Routes} from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
 import { useTelegram } from './components/hooks/useTelegram';
-import {First_frame} from './components/First_frame/First_frame'
+import {First_frame} from './components/first_frame/First_frame'
 
 function App() {
 
