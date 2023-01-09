4c39f160facaeb67c7715d0942447571aa0f8076
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-09 11:32:13 +0200

    fix redirect

--- antomapp/src/components/FirstFrame/FirstFrame.jsx
+++ antomapp/src/components/FirstFrame/FirstFrame.jsx
@@ -1,9 +1,56 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+import Button from '../Button/Button';
+
+
+// const goMenu = () => {
+//     return <redirect to='/'/>
+//   };
+
+// const goOrder = () => {
+//     return <redirect to='/order'/>
+//   };
+
+// const goAuth = () => {
+//     return <redirect to='/form'/>
+//   };
+
+// const Menu = () =>{ 
+//     let path = `/`; 
+//     navigate(path);
+//   }
+
+// const Order = () =>{ 
+//     let path = `/order`; 
+//     navigate(path);
+//   }  
+
+// const Auth = () =>{ 
+//     let path = `/form`; 
+//     navigate(path);
+//   }    
 const FirstFrame  = () => {
+    let navigate = useNavigate();
+
+    const Menu = () =>{ 
+        let path = `/`; 
+        navigate(path);
+      }
+    
+    const Order = () =>{ 
+        let path = `/order`; 
+        navigate(path);
+      }  
+    
+    const Auth = () =>{ 
+        let path = `/form`; 
+        navigate(path);
+      }       
     return (
         <ul>
-            <li><button variant="contained"><a href="/">Menu</a></button></li>
-            <li><button variant="contained"><a href="/form">Регистрация</a></button></li>
-            <li><button variant="contained"><a href="/order">Order</a></button></li>
+            <Button color="primary" className="px-4" onClick={Menu}>Menu</Button>
+            <Button color="primary" className="px-4" onClick={Order}>Order</Button>
+            <Button color="primary" className="px-4" onClick={Auth}>Auth</Button>
         </ul>
     );
 }
--- antomapp/src/components/Header/Header.jsx
+++ antomapp/src/components/Header/Header.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import Button from "../Button/Button";
 import {useTelegram} from "../hooks/useTelegram";
 import './Header.css'
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
     const {user, onClose} = useTelegram();
+    let navigate = useNavigate();
+
+    const Menu = () =>{ 
+        let path = `/`; 
+        navigate(path);
+      }
 
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Close me</Button>
-            <a href="/"><Button>Menu</Button></a>
+            <Button onClick={Menu}>Menu</Button>
             <span className={'username'}>
                 {user?.username}
             </span>
