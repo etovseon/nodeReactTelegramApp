9890b381447de58f08f98d876056f244e4e816fe
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-09 09:18:08 +0200

    test

--- antomapp/src/components/FirstFrame/FirstFrame.jsx
+++ antomapp/src/components/FirstFrame/FirstFrame.jsx
@@ -2,7 +2,7 @@ const FirstFrame  = () => {
     return (
     <div className={"FirstFrame"}>
         <ul>
-            <li><a href='/order'>order</a></li>
+            <li><a href='/order'>order1</a></li>
             <li><a href='/last_orer'>last order</a></li>
             <li><a href='/'>Product List</a></li>
             <li><a href='/form'>Анкета</a></li>
